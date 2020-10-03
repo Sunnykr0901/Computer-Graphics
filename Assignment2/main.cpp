fc0f33b6e4a28c57fdb0c2911fde0f9d877c5acd
Sunny <sunnykr1530@gmail.com>
2020-10-03 22:12:44 +0530

    assignment 2 added

---
+++ Assignment2/main.cpp
@@ -0,0 +1,145 @@
+/*
+Name-Sunny Kumar
+Roll No-170101068
+*/
+#include<bits/stdc++.h>
+#include<GL/glut.h>
+#include<stdio.h>
+using namespace std;
+
+bool mouseLeftDown,mouseRightDown;
+float mouseX, mouseY;
+float cameraAngleX = 0,cameraAngleY = 0;
+static int lastx = 0, lasty = 0; 
+static GLfloat  scaling = 4.0f;
+vector<vector<double>>vertices;         //store vertices
+vector<vector<int>>faces;               //store faces as indices of three vertices of a triangle
+
+//load all vertices and faces
+void objloader(){
+    string line;
+    ifstream readfile("lowpolybunny.obj");
+    while(getline(readfile,line)){
+        istringstream ss(line);
+        vector<int>t1;
+        vector<double>t2;
+        string flag;
+        ss>>flag;
+        while(ss){
+            double word;
+            ss>>word;
+            if(flag=="v")t2.push_back(word);
+            else if(flag=="f")t1.push_back(((int)word)-1);
+        }
+        if(flag=="v")vertices.push_back(t2);
+        else if(flag=="f")faces.push_back(t1);
+    }
+}
+
+void mouseCB(int button, int state, int x, int y){
+
+    mouseX = x;
+    mouseY = y;
+    if (button == GLUT_LEFT_BUTTON){
+        if(state==GLUT_DOWN)mouseLeftDown=true;
+        else if(state==GLUT_UP)mouseLeftDown=false;
+    }
+    else if(button==GLUT_RIGHT_BUTTON){
+        if(state==GLUT_DOWN)mouseRightDown=true;
+        else if(state==GLUT_UP)mouseRightDown=false;
+    }
+}
+
+void mouseMotionCB(int x, int y){
+    
+    int movx, movy;
+    movx = lastx - x;
+    movy = lasty - y;
+
+    if (mouseLeftDown || mouseRightDown){
+        cameraAngleY += (x - mouseX);
+        cameraAngleX += (y - mouseY);
+        mouseX = x;
+        mouseY = y;
+    }
+    if ( abs(movy) < 10 ){ scaling  += (float)(movy) / 100.0f;  }
+    if ( scaling < 00.1f) scaling = 0.1f;
+    if ( scaling > 10.0f) scaling = 10.0f; 
+    lastx = x;
+    lasty = y;
+    glutPostRedisplay();
+}
+
+void initial(){
+    mouseLeftDown = mouseRightDown = false;
+}
+
+void resize(int w, int h){
+
+    glViewport(0, 0, w, h);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(30.0, (double)w / (double)h, 1.0, 100.0);
+    glMatrixMode(GL_MODELVIEW);
+}
+
+// main display function
+void draw(){
+
+    glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(3.0, 4.0, 5.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+    glPushMatrix();
+    glRotatef(cameraAngleX, 1, 0, 0);   
+    glRotatef(cameraAngleY, 0, 1, 0);  
+    glScalef(scaling, scaling, scaling);
+    glColor3f(0.8,0.8,0.8);
+    glBegin(GL_TRIANGLES);
+    for(int i=0;i<faces.size();i++){
+        vector<double>v=vertices[faces[i][0]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][1]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][2]];
+        glVertex3f(v[0],v[1],v[2]);
+    }
+    glEnd();
+
+    glLineWidth(1.0);
+    glColor3f(0.0,0.0,0.0);
+    glBegin(GL_LINES);
+    for(int i=0;i<faces.size();i++){
+        vector<double>v=vertices[faces[i][0]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][1]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][1]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][2]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][2]];
+        glVertex3f(v[0],v[1],v[2]);
+        v=vertices[faces[i][0]];
+        glVertex3f(v[0],v[1],v[2]);
+    }
+    glEnd();
+    glPopMatrix();
+    glFlush();
+}
+
+int main(int argc,char** argv){
+    objloader();
+    glutInit(&argc, argv);
+    initial();
+    glutInitDisplayMode(GLUT_RGBA | GLUT_SINGLE|GLUT_DEPTH);
+    glutInitWindowSize(1000, 1000);
+    glutInitWindowPosition(100, 100);
+    glutCreateWindow("3D Rotating Mesh");
+    glEnable(GL_DEPTH_TEST);
+    glutDisplayFunc(draw);
+    glutReshapeFunc(resize);
+    glutMouseFunc(mouseCB);
+    glutMotionFunc(mouseMotionCB);
+    glutMainLoop();
+    return 0;
+}
